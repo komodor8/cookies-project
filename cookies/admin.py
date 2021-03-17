75913881588dd85d698eeff769c426c09fa2f03f
= <=>
2021-03-17 15:45:31 +0100

    first commit

---
+++ cookies/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Author, Book
+
+
+# Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
---
+++ cookies/views.py
@@ -0,0 +1,8 @@
+from django.views import generic
+
+
+# Create your views here.
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'cookies/index.html'

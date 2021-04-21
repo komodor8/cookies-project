7bbdb05fffa0ac136e00b20567a22da902f2de83
komodor8 <k.modor@yahoo.com>
2021-04-21 11:37:08 +0200

    Update settings.py

--- cookiesite/settings.py
+++ cookiesite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'lu)oc#fmp4fz2h(lo$d=r^p*=&jv1+n=frn%+$rbdn#z$h4ho$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
